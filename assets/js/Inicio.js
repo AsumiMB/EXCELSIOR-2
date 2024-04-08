b16f1e228272806abb292678b5a2821e0e829eae
EVELIN <tapia2007evelin@gmail.com>
2024-04-08 18:25:03 -0500

    first commit

---
+++ assets/js/Inicio.js
@@ -0,0 +1,7 @@
+document.getElementById("miBoton").addEventListener("mousedown", function() {
+    this.style.backgroundColor = "rgba(255, 255, 255, 0.5)"; /* Cambia el color de fondo al presionar */
+});
+
+document.getElementById("miBoton").addEventListener("mouseup", function() {
+    this.style.backgroundColor = "transparent"; /* Restaura el color de fondo cuando se suelta el botón */
+});
